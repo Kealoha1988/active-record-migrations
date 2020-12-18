c66e814d3e14e1db34497bbf457ef32a9eabd0c3
Kealoha1988 <kealohaabe1988@gmail.com>
2020-12-18 13:18:30 -0500

    commit tree 6a4c0e74c96618fc9735cb68746ceeb967a3e03a

---
+++ db/migrate/01_create_artists.rb
@@ -0,0 +1,18 @@
+class CreateArtists < ActiveRecord::Migration[4.2]
+    def up
+      rake db:migrate
+    end
+  
+    def down
+      rake db:rollback
+    end
+
+    def change
+      create_table :artists do |t|
+        t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+      end
+    end
+  end 
